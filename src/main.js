6c297134d16f98b1826afac5474ed2baab2e8ded
xanderflores <alexanderflores_10_@hotmail.es>
2024-03-15 21:46:11 -0300

    a

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueRouter from 'vue-router';
 
-// Importa los componentes para las diferentes rutas
-import Contacto from './Pages/Contacto/ContactoComp.vue';
 
 
 Vue.config.productionTip = false
@@ -16,19 +13,13 @@ Vue.use(BootstrapVue)
 
 
 // Configura las rutas
-const routes = [
-  { path: '/', component: App },
-  { path: '/contacto', component: Contacto}
-];
+
 
 // Crea una instancia de VueRouter y pasa las rutas
-const router = new VueRouter({
-  routes
-});
 
 
 // Asocia el enrutador con la instancia principal de Vue
 new Vue({
-  router,
+ 
   render: h => h(App),
 }).$mount('#app')
